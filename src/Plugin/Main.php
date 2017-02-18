c3e4e17b0a4a7933e12f26f609755748b63c5605
JunkDaCoder <junkdudeofficial@gmail.com>
2017-02-17 23:23:28 -0700

    Changed to what i use

--- src/Plugin/Main.php
+++ src/Plugin/Main.php
@@ -1,26 +1,55 @@
 <?php
-
-namespace YourPlugin;
-
-use pocketmine\plugin\PluginBase; #Required
-use pocketmine\command\CommandSender; #To know the command sender
-use pocketmine\command\Command; #To be able to add commands
-use pocketmine\utils\Config; #For Config
-use pocketmine\utils\TextFormat as C; #For Colors with the "C" format instead of "TextFormat"
+namespace Junkdude;
+use pocketmine\block\Block;
+use pocketmine\event\player\{
+    PlayerGameModeChangeEvent, PlayerChatEvent, PlayerCommandPreprocessEvent, PlayerDeathEvent, PlayerHungerChangeEvent, PlayerInteractEvent, PlayerItemConsumeEvent, PlayerItemHeldEvent, PlayerLoginEvent, PlayerQuitEvent, PlayerDropItemEvent, PlayerJoinEvent
+};
+use pocketmine\event\entity\{
+    EntityArmorChangeEvent, EntityDamageByEntityEvent, EntityDamageEvent, EntityTeleportEvent, EntityExplodeEvent, ExplosionPrimeEvent, EntitySpawnEvent, EntityRegainHealthEvent
+};
+use pocketmine\level\particle\{
+    AngryVillagerParticle, BubbleParticle, CriticalParticle, DustParticle, EnchantParticle, EnchantmentTableParticle, ExplodeParticle, FlameParticle, FloatingTextParticle, GenericParticle, HappyVillagerParticle, HeartParticle, HugeExplodeParticle, InkParticle, InstantEnchantParticle, ItemBreakParticle, LargeExplodeParticle, LavaDripParticle, LavaParticle, MobSpawnParticle, Particle, PortalParticle, RedstoneParticle, SmokeParticle, SplashParticle, SporeParticle, TerrainParticle, WaterDripParticle, WaterParticle
+};
 use pocketmine\event\Listener;
-
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\Server;
+use pocketmine\nbt\NBT;
+use pocketmine\level\format\FullChunk;
+    use pocketmine\nbt\tag\ByteTag;
+    use pocketmine\nbt\tag\CompoundTag;
+    use pocketmine\nbt\tag\DoubleTag;
+    use pocketmine\nbt\tag\FloatTag;
+    use pocketmine\nbt\tag\IntTag;
+    use pocketmine\nbt\tag\ListTag;
+    use pocketmine\nbt\tag\ShortTag;
+    use pocketmine\nbt\tag\StringTag;
+    use pocketmine\nbt\tag\IntArrayTag;
+    use pocketmine\entity\Villager;
+    use pocketmine\entity\Zombie;
+use pocketmine\utils\Config;
+use pocketmine\tile\{
+    Tile, Chest, MobSpawner
+};
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\command\{
+    Command, CommandSender, ConsoleCommandSender
+};
+use Junkdude\Hud;
+use Junkdude\CEChecker;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\entity\{
+    Creature, Effect, Entity, Human, Item as EntityItem, Living
+};
+use pocketmine\inventory\ShapedRecipe;
+use pocketmine\inventory\BrewingRecipe;
+use pocketmine\event\block\SignChangeEvent;
 class Main extends PluginBase implements Listener{
-    
-    public function onEnable(){
-        $this->getLogger()->info("Plugin enabled!");
-        $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this,$this);
-        $this->saveResource("config.yml");
-    }
-    public function onDisable(){
-        $this->getLogger()->info("Plugin disabled!");
-    }
-    public function onLoad(){
-        $this->getLogger()->info("Loading Plugin!");
-    }
 }
